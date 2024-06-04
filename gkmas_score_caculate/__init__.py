d99b9e4d4d8db0b31ae58bbb70a3b84bed4c920b
夏莫 <2838596569@qq.com>
2024-06-04 13:16:17 +0800

    取消5w分限制，给面板就算

--- gkmas_score_caculate/__init__.py
+++ gkmas_score_caculate/__init__.py
@@ -17,7 +17,7 @@ def score_caculate(score:int):
     elif score <= 3050:return int((score-2250)/0.08) + 10000
     elif score <= 3450:return int((score-3050)/0.04) + 20000
     elif score <= 3650:return int((score-3450)/0.02) + 30000
-    elif score <= 3750:return int((score-3650)/0.01) + 40000
+    else:return int((score-3650)/0.01) + 40000
 
 @sv.on_prefix('算分','查分')
 async def gkmas_score_caculate(bot,ev):
@@ -39,10 +39,8 @@ async def gkmas_score_caculate(bot,ev):
     if 1500-vi <=30:flu += vi-1470
     msg = f'您的面板为{state}→{state+90-flu}(+{90-flu})\n'
     score = 1700 + int((state+90-flu)*2.3)
-    if score < 6250:msg += '离A还很遥远哦，再接再厉吧！'
-    elif score < 7750:msg += f'A评价需要最终试验获得{score_caculate(10000-score)}分\n离A+还很遥远哦，再接再厉吧！'
-    elif score < 8200:msg += f'A评价需要最终试验获得{score_caculate(10000-score)}分\nA+评价需要最终试验获得{score_caculate(11500-score)}分\n离S还很遥远哦，再接再厉吧！'
-    elif score < 9250:msg += f'最终试验1位即可达成A评价\nA+评价需要最终试验获得{score_caculate(11500-score)}分\n离S还很遥远哦，再接再厉吧！'
+    if score < 8200:msg += f'A评价需要最终试验获得{score_caculate(10000-score)}分\nA+评价需要最终试验获得{score_caculate(11500-score)}分\nS评价需要最终试验获得{score_caculate(13000-score)}分'
+    elif score < 9250:msg += f'最终试验1位即可达成A评价\nA+评价需要最终试验获得{score_caculate(11500-score)}分\nS评价需要最终试验获得{score_caculate(13000-score)}分'
     elif score < 9700:msg += f'A+评价需要最终试验获得{score_caculate(11500-score)}分\nS评价需要最终试验获得{score_caculate(13000-score)}分'
     elif score < 11200:msg += f'最终试验1位即可达成A+评价\nS评价需要最终试验获得{score_caculate(13000-score)}分'
     else:msg += f'最终试验1位即可达成S评价'
