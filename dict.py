3632cae8ee0107a10ab44d19a693ccc9682b73d3
夏莫 <2838596569@qq.com>
2024-06-22 17:57:24 +0800

    更新功能

--- dict.py
+++
@@ -1,4 +0,0 @@
-#定义字典类，用于查询游戏内名词
-class Dict:
-    def __init__(self):
-        self.dict = {}
--- gkmas_score_caculate/__init__.py
+++ gkmas_score_caculate/__init__.py
@@ -1,15 +1,7 @@
 #算分
 
 from hoshino import Service
-
-sv_help = """
-学马仕功能
-通过面板计算达到对应评价需要的分数
-格式为：算分 vo da vi
-(注意：这里的面板为进入最终试验之前的面板)
-"""
-
-sv = Service('gkmas_score_caculate',help_=sv_help)
+sv = Service('gkmas_score_caculate')
 
 def score_caculate(score:int):
     if score <= 1500:return int(score/0.3)
@@ -28,7 +20,7 @@ async def gkmas_score_caculate(bot,ev):
         for i in data:
             if not i.isdigit():await bot.send(ev,'格式错误，应为[算分 vo da vi]');return
             data[n] = int(i)
-            if int(i)>1500 or int(i)<0:await bot.send(ev,'属性值不合理，应在1~1500区间内');return
+            if int(i)>1500 or int(i)<1:await bot.send(ev,'属性值不合理，应在1~1500区间内');return
             n += 1
     else: await bot.send(ev,'格式错误，应为[算分 vo da vi]');return
     vo,da,vi = data
@@ -45,3 +37,104 @@ async def gkmas_score_caculate(bot,ev):
     elif score < 11200:msg += f'最终试验1位即可达成A+评价\nS评价需要最终试验获得{score_caculate(13000-score)}分'
     else:msg += f'最终试验1位即可达成S评价'
     await bot.send(ev,msg)
+
+#以下假设第一名为7000分(1800)，第二名为5000分(1500)，第三名为2500(750)分
+
+rank2score = {1:1700,2:900,3:500,4:0}
+
+@sv.on_prefix('逆算分')
+async def gkmas_score_in_caculate(bot,ev):
+    texts:str = ev.message.extract_plain_text().strip()
+    data = texts.split(' ')
+    if len(data) == 4:
+        n = 0
+        for i in data:
+            if not i.isdigit():await bot.send(ev,'格式错误，应为[逆算分 vo da vi 最终评价分]');return
+            data[n] = int(i)
+            if int(i)>1500 or int(i)<1:
+                if n <= 3:await bot.send(ev,'属性值不合理，应在1~1500区间内');return
+            n += 1
+    else: await bot.send(ev,'格式错误，应为[逆算分 vo da vi 最终评价分]');return
+    vo,da,vi,score_r = data
+    state = vo+da+vi
+    score = int(state*2.3)
+    diff = score_r - score
+    if diff < 0:await bot.send(ev,'最终评价分不合理');return
+    elif diff< 750:rank = 4
+    elif diff<1500:rank = 3
+    elif diff<1800:rank = 2
+    else:rank = 1
+    msg = f'您的面板为{state}\n最终试验取得了:\n    {score_caculate(diff+rank2score[rank])}~{score_caculate(diff+1+rank2score[rank])-1}分({rank}位)\n(pro模式适用)'
+    await bot.send(ev,msg)
+
+@sv.on_prefix('算目标分')
+async def gkmas_score_ta_caculate(bot,ev):
+    texts:str = ev.message.extract_plain_text().strip()
+    data = texts.split(' ')
+    if len(data) == 4:
+        n = 0
+        for i in data:
+            if not i.isdigit():await bot.send(ev,'格式错误，应为[算目标分 vo da vi 最终评价分]');return
+            data[n] = int(i)
+            if int(i)>1500 or int(i)<1:
+                if n <= 3:await bot.send(ev,'属性值不合理，应在1~1500区间内');return
+            n += 1
+    else: await bot.send(ev,'格式错误，应为[算目标分 vo da vi 最终评价分]');return
+    vo,da,vi,score_r = data
+    state = vo+da+vi
+    flu = 0
+    if 1500-vo <=30:flu += vo-1470
+    if 1500-da <=30:flu += da-1470
+    if 1500-vi <=30:flu += vi-1470
+    msg = f'您的面板为{state}→{state+90-flu}(+{90-flu})\n'
+    score = int((state+90-flu)*2.3)
+    diff = score_r - score
+    if diff < 0:await bot.send(ev,'已达到目标评价！\n(仅pro模式适用)');return
+    elif diff< 750:rank = 4
+    elif diff<1500:rank = 3
+    elif diff<1800:rank = 2
+    else:rank = 1
+    msg = f'达到目标评价需要在最终试验获得:\n    {score_caculate(diff+rank2score[rank])}~{score_caculate(diff+1+rank2score[rank])-1}分({rank}位)\n(仅pro模式适用)'
+    await bot.send(ev,msg)
+
+@sv.on_prefix('算加练')
+async def gkmas_oiko_caculate(bot,ev):
+    texts:str = ev.message.extract_plain_text().strip()
+    data = texts.split(' ')
+    err = '格式错误，应为[算加练 vo vo% da da% vi vi%]\n或[算加练 某一属性 该属性加成%](%可省略)'
+    if len(data) == 2:
+        if not data[0].isdigit():await bot.send(ev,err);return
+        state = int(data[0])
+        if data[1].endswith('%'):
+            state_p = data[1][:-1]
+        else:state_p = data[1]
+        if not state_p.isdigit():await bot.send(ev,err);return
+        state_p = int(state_p)
+        state_end_1 = state + int(state_p*310/100)
+        if state_end > 1500:state_end = 1500
+        state_end_2 = state + int(state_p*145/100)
+        await bot.finish(ev,f'主训练→{state_end_1}\n副训练→{state_end_2}');return
+    elif len(data) == 6:
+        vo,vo_p,da,da_p,vi,vi_p = data
+        for i in data:
+            if not i.isdigit() or (i.endswith('%') and not i[:-1].isdigit()):await bot.send(ev,err);return
+        if vo_p.endswith('%'):vo_p = vo_p[:-1]
+        if da_p.endswith('%'):da_p = da_p[:-1]
+        if vi_p.endswith('%'):vi_p = vi_p[:-1]
+        vo, vo_p, da, da_p, vi, vi_p = map(int, [vo, vo_p, da, da_p, vi, vi_p])
+        f1 = lambda x,y:x+int(y*310/100) if x+int(y*310/100)<1500 else 1500
+        f2 = lambda x,y:x+int(y*145/100) if x+int(y*145/100)<1500 else 1500
+        f3 = lambda x:x+30 if x+30<1500 else 1500
+        state_end_vo = f1(vo,vo_p) + f2(da,da_p) + f2(vi,vi_p)
+        _state_end_vo = f3(f1(vo,vo_p)) + f3(f2(da,da_p)) + f3(f2(vi,vi_p))
+        state_end_da = f2(vo,vo_p) + f1(da,da_p) + f2(vi,vi_p)
+        _state_end_da = f3(f2(vo,vo_p)) + f3(f1(da,da_p)) + f3(f2(vi,vi_p))
+        state_end_vi = f2(vo,vo_p) + f2(da,da_p) + f1(vi,vi_p)
+        _state_end_vi = f3(f2(vo,vo_p)) + f3(f2(da,da_p)) + f3(f1(vi,vi_p))
+        state = {"vo":state_end_vo,"da":state_end_da,"vi":state_end_vi}
+        _state = {"vo":_state_end_vo,"da":_state_end_da,"vi":_state_end_vi}
+        trt = {"vo":[f1,f2,f2],"da":[f2,f1,f2],"vi":[f2,f2,f1]}
+        max_state = max(_state,key=_state.get)
+        msg = f'建议选择{max_state}训练\n训练后面板:{state[max_state]}→{_state[max_state]}\nvo:{vo}→{trt[max_state][0](vo,vo_p)}\nda:{da}→{trt[max_state][1](da,da_p)}\nvi:{vi}→{trt[max_state][2](vi,vi_p)}\n(未考虑s卡加值)'
+        await bot.finish(ev,msg)
+    else: await bot.send(ev,err);return
